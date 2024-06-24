389290594b170379da661bccabec441f27a84189
fhAnso <felix.hansow@proton.me>
2024-06-24 10:41:07 +0200

    Initialized confwrite
    Created confwrite.c and confwrite.h

---
+++ confparse/confwrite.c
@@ -0,0 +1,16 @@
+/*
+ * confparse - small config file parser library, v1.0.6
+ *
+ * The confparse project facilitates the process of parsing a configuration
+ * file into keys and their corresponding values. These pairs can be handled
+ * individually, allowing for easy reading of configuration files. Currently,
+ * this project supports up to 500 entries.
+ *
+ * Author: fhAnso
+ * License: MIT
+ * GitHub Repository: https://github.com/fhAnso/confparse
+ */
+
+#include "confwrite.h"
+#include "confparse.h"
+
---
+++ confparse/confwrite.h
@@ -0,0 +1,18 @@
+/*
+ * confparse - small config file parser library, v1.0.6
+ *
+ * The confparse project facilitates the process of parsing a configuration
+ * file into keys and their corresponding values. These pairs can be handled
+ * individually, allowing for easy reading of configuration files. Currently,
+ * this project supports up to 500 entries.
+ *
+ * Author: fhAnso
+ * License: MIT
+ * GitHub Repository: https://github.com/fhAnso/confparse
+ */
+
+#ifndef CONFWRITE_H
+#define CONFWRITE_H
+
+
+#endif
